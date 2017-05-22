ab868e9ace819170b1b9dc9a26ba65d6fc010b5f
nikitatroshenko <nikitatroshenko@gmail.com>
2017-05-22 19:21:59 +0300

    Added lab 3

---
+++ PictureDiagramWinForms/PictureDiagramWinForms/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace PictureDiagramWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartRed = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chartGreen = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartBlue = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.averageRedLabel = new System.Windows.Forms.Label();
+            this.averageGreenLabel = new System.Windows.Forms.Label();
+            this.averageBlueLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartRed
+            // 
+            chartArea1.Name = "chartArea";
+            this.chartRed.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartRed.Legends.Add(legend1);
+            this.chartRed.Location = new System.Drawing.Point(462, 69);
+            this.chartRed.Name = "chartRed";
+            this.chartRed.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chartRed.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Red};
+            series1.ChartArea = "chartArea";
+            series1.Legend = "Legend1";
+            series1.Name = "Red";
+            this.chartRed.Series.Add(series1);
+            this.chartRed.Size = new System.Drawing.Size(375, 168);
+            this.chartRed.TabIndex = 0;
+            this.chartRed.Text = "chart";
+            this.chartRed.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(443, 516);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chartGreen
+            // 
+            chartArea2.Name = "chartArea";
+            this.chartGreen.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartGreen.Legends.Add(legend2);
+            this.chartGreen.Location = new System.Drawing.Point(462, 243);
+            this.chartGreen.Name = "chartGreen";
+            this.chartGreen.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chartGreen.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Green};
+            series2.ChartArea = "chartArea";
+            series2.Legend = "Legend1";
+            series2.Name = "Green";
+            this.chartGreen.Series.Add(series2);
+            this.chartGreen.Size = new System.Drawing.Size(375, 168);
+            this.chartGreen.TabIndex = 4;
+            this.chartGreen.Text = "chart1";
+            // 
+            // chartBlue
+            // 
+            chartArea3.Name = "chartArea";
+            this.chartBlue.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartBlue.Legends.Add(legend3);
+            this.chartBlue.Location = new System.Drawing.Point(462, 417);
+            this.chartBlue.Name = "chartBlue";
+            this.chartBlue.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chartBlue.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Blue};
+            series3.ChartArea = "chartArea";
+            series3.Legend = "Legend1";
+            series3.Name = "Blue";
+            this.chartBlue.Series.Add(series3);
+            this.chartBlue.Size = new System.Drawing.Size(375, 168);
+            this.chartBlue.TabIndex = 5;
+            this.chartBlue.Text = "chart2";
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(189, 32);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(0, 13);
+            this.fileNameLabel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(858, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Average";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(858, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Average";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(858, 427);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Average";
+            // 
+            // averageRedLabel
+            // 
+            this.averageRedLabel.AutoSize = true;
+            this.averageRedLabel.Location = new System.Drawing.Point(858, 110);
+            this.averageRedLabel.Name = "averageRedLabel";
+            this.averageRedLabel.Size = new System.Drawing.Size(0, 13);
+            this.averageRedLabel.TabIndex = 9;
+            // 
+            // averageGreenLabel
+            // 
+            this.averageGreenLabel.AutoSize = true;
+            this.averageGreenLabel.Location = new System.Drawing.Point(858, 291);
+            this.averageGreenLabel.Name = "averageGreenLabel";
+            this.averageGreenLabel.Size = new System.Drawing.Size(0, 13);
+            this.averageGreenLabel.TabIndex = 10;
+            // 
+            // averageBlueLabel
+            // 
+            this.averageBlueLabel.AutoSize = true;
+            this.averageBlueLabel.Location = new System.Drawing.Point(858, 460);
+            this.averageBlueLabel.Name = "averageBlueLabel";
+            this.averageBlueLabel.Size = new System.Drawing.Size(0, 13);
+            this.averageBlueLabel.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(965, 593);
+            this.Controls.Add(this.averageBlueLabel);
+            this.Controls.Add(this.averageGreenLabel);
+            this.Controls.Add(this.averageRedLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chartBlue);
+            this.Controls.Add(this.chartGreen);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chartRed);
+            this.Name = "Form1";
+            this.Text = "Diagram";
+            ((System.ComponentModel.ISupportInitialize)(this.chartRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRed;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartGreen;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartBlue;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label averageRedLabel;
+        private System.Windows.Forms.Label averageGreenLabel;
+        private System.Windows.Forms.Label averageBlueLabel;
+    }
+}
+
---
+++ PictureDiagramWinForms/PictureDiagramWinForms/Form1.cs
@@ -0,0 +1,134 @@
+﻿using AForge.Imaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PictureDiagramWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //CreateChart();
+        }
+
+        public void CreateChart()
+        {
+            int n = 3;
+            double[] redValues = { 0.1, 0.8, 0.5 };
+            //this.chart.Series["serias"].Points.AddXY("Max", redValues);
+            for (int i = 0; i < n; i++)
+            {
+                this.chartRed.Series["Red"].Points.Add(redValues[i]);
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        System.Drawing.Image inputImage;
+        Bitmap sourceBitmap;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = new Bitmap(dlg.FileName);
+                    inputImage = pictureBox1.Image;
+                    fileNameLabel.Text = dlg.FileName;
+                }
+            }
+            if (inputImage != null)
+            {
+                sourceBitmap = new Bitmap(inputImage);
+
+                this.chartRed.Series["Red"].Points.Clear();
+                this.chartGreen.Series["Green"].Points.Clear();
+                this.chartBlue.Series["Blue"].Points.Clear();
+
+
+                //createDiagramAutomatic();
+                createDiagramManual();
+            }
+            
+        }
+
+        private void createDiagramManual()
+        {
+            int[] redValues = new int[256];
+            int[] greenValues = new int[256];
+            int[] blueValues = new int[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                redValues[i] = 0;
+                greenValues[i] = 0;
+                blueValues[i] = 0;
+            }
+
+            double averageRed = 0.0;
+            double averageGreen = 0.0;
+            double averageBlue = 0.0;
+
+            for (int i = 0; i < sourceBitmap.Width; i++)
+            {
+                for (int j = 0; j < sourceBitmap.Height; j++)
+                {
+                    System.Drawing.Color c = sourceBitmap.GetPixel(i, j);
+                    redValues[c.R]++;
+                    greenValues[c.G]++;
+                    blueValues[c.B]++;
+                    averageRed += c.R;
+                    averageGreen += c.G;
+                    averageBlue += c.B;
+                }
+            }
+
+            averageRed = averageRed / (double)(sourceBitmap.Width * sourceBitmap.Height);
+            averageGreen = averageGreen / (double)(sourceBitmap.Width * sourceBitmap.Height);
+            averageBlue = averageBlue / (double)(sourceBitmap.Width * sourceBitmap.Height);
+
+            averageRedLabel.Text = ((int)averageRed).ToString();
+            averageGreenLabel.Text = ((int)averageGreen).ToString();
+            averageBlueLabel.Text = ((int)averageBlue).ToString();
+
+            for (int k = 0; k < 256; k++)
+            {
+                this.chartRed.Series["Red"].Points.Add(redValues[k]);
+                this.chartGreen.Series["Green"].Points.Add(greenValues[k]);
+                this.chartBlue.Series["Blue"].Points.Add(blueValues[k]);
+            }
+        }
+
+        private void createDiagramAutomatic()
+        {
+            ImageStatistics rgbStatistics = new ImageStatistics(sourceBitmap);
+            int[] redValues = rgbStatistics.Red.Values;
+            int[] greenValues = rgbStatistics.Green.Values;
+            int[] blueValues = rgbStatistics.Blue.Values;
+
+            for (int k = 0; k < 256; k++)
+            {
+                this.chartRed.Series["Red"].Points.Add(redValues[k]);
+                this.chartGreen.Series["Green"].Points.Add(greenValues[k]);
+                this.chartBlue.Series["Blue"].Points.Add(blueValues[k]);
+            }
+        }
+
+    }
+}
