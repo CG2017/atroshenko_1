6818da0359e5c6c7ca5aae426dcd02c25c8d123e
nikitatroshenko <nikitatroshenko@gmail.com>
2017-05-29 19:11:21 +0300

    Added lab 4

---
+++ ImageInfoWinForms/ImageInfoWinForms/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ImageInfoWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Location = new System.Drawing.Point(12, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.selectFilesButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightGray;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(570, 412);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Name";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Dimension";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "ResolutionX";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "ResolutionY";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Compression";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(594, 492);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ ImageInfoWinForms/ImageInfoWinForms/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using MetadataExtractor;
+using Directory = System.IO.Directory;
+
+namespace ImageInfoWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void selectFilesButton_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+
+            string folderName = "";
+
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderName = folderBrowserDialog1.SelectedPath;
+                string[] dirs = Directory.GetFiles(folderName);
+                Regex regex = new Regex("(.TIF)|(.BMP)|(.JPG)|(.GIF)|(.PNG)$", RegexOptions.IgnoreCase);
+               
+                if (dirs != null)
+                {
+                    int i = 0;
+                    Image loadedImage;
+                    foreach (String file in dirs)
+                    {
+                        if (regex.IsMatch(file) && i < dirs.Length)
+                        {
+                            loadedImage = Image.FromFile(file);
+                            string[] row = GetMetaData(file);
+
+                            if (row[4] == null || row[4].Length == 0)
+                            {
+                                row[4] = getCompression(loadedImage);
+                            }
+
+                            row[2] = Math.Round(loadedImage.HorizontalResolution).ToString();
+                            row[3] = Math.Round(loadedImage.VerticalResolution).ToString();
+
+                            dataGridView1.Rows.Add(row);
+                            i++;
+                        }
+                    }
+                }
+                
+            }
+        }
+
+        private string[] GetMetaData(string fileName)
+        {
+            var metadata = ImageMetadataReader.ReadMetadata(fileName);
+            var result = new string[5];
+            string height = "";
+            string width = "";
+
+            foreach (var directory in metadata)
+            {
+                foreach (var directoryTag in directory.Tags)
+                {
+                    switch (directoryTag.Name)
+                    {
+                    case "Compression Type":
+                        result[4] = directoryTag.Description;
+                        break;
+                    case "File Name":
+                        result[0] = directoryTag.Description;
+                        break;
+                    case "Image Height":
+                        height = directoryTag.Description.Split(' ')[0];
+                        break;
+                    case "Image Width":
+                        width = directoryTag.Description.Split(' ')[0];
+                        break;
+                    }
+                }
+            }
+
+            result[1] = string.Format("{0}x{1}", width, height);
+
+            return result;
+        }
+
+        string getCompression(Image image)
+        {
+            int compressionTagIndex = Array.IndexOf(image.PropertyIdList, 0x103);
+            int Type = 0;
+            if (compressionTagIndex > -1)
+            {
+                PropertyItem compressionTag = image.PropertyItems[compressionTagIndex];
+                Type = BitConverter.ToUInt16(compressionTag.Value, 0);
+            }
+            string Result = "No compression";
+            switch (Type)
+            {
+                case 2:
+                    Result = "CCITT modified Huffman RLE";
+                    break;
+                case 3:
+                    /* falls through */
+                case 32771:
+                    Result = "CCITT Group 3 fax encoding";
+                    break;
+                case 4:
+                    Result = "CCITT Group 4 fax encoding";
+                    break;
+                case 5:
+                    Result = "LZW";
+                    break;
+                case 6:
+                    Result = "'old-style' JPEG";
+                    break;
+                case 7:
+                    Result = "'new-style' JPEG";
+                    break;
+                case 32773:
+                    Result = "Macintosh RLE";
+                    break;
+            }
+            return Result;
+        }
+    }
+}
